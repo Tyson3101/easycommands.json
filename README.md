67594d1b7fc1229550652e7f1cb8625361d16c41
Tyson3101 <67045138+Tyson3101@users.noreply.github.com>
2020-12-11 03:44:07 +1100

    Major additions

--- README.md
+++ README.md
@@ -2,13 +2,21 @@
 
 ## Description
 
-### easycommands.json is an easy way to make commands by using a JSON file with input and output key value pairs instead of using a shit ton of if statements or switch cases that fill up your code by an unnecessary amount.
+### easycommands.json is an easy way to make discord commands by using a JSON file with input and output key value pairs instead of using a shit ton (Don't deny it) of if statements or switch cases that fill up your code by an unnecessary amount.
 
 ## Example
 
 ##### Note: init refers to how you defined the easycommands.json module, which is what the example shows.
 
-### Firstly import the easycommands.json module
+##### Note: Can use object instead of JSON File.
+
+### Firstly install the module
+
+```js
+npm i easycommands.json
+```
+
+### Secondly import the easycommands.json module
 
 ```js
 // ES6 || TypeScript
@@ -18,7 +26,19 @@ import init from "easycommands.json";
 const init = require("easycommands.json");
 ```
 
-### Secondly make the JSON file
+## Short Info
+
+### There are 2 approaches to the easy commands.
+
+#### First way being to make a JSON File(s) which will be shown below. - Better because is a file, seperated from code. Easier and more simple to work with as a whole and for beginners. Unless need somewhat complicated commands. JSON is the way to go.
+
+###### OR
+
+#### Make an object(s) with the same format as the JSON File. - Better because have better functionatly, can make input be an array from other packages. Use of typescript interface which make it easier to work with.
+
+## JSON File
+
+### First make the JSON file
 
 ```js
 {
@@ -27,7 +47,7 @@ const init = require("easycommands.json");
 }
 ```
 
-### Both of noPrefix and prefix have the same properties
+#### Both of noPrefix and prefix have the same properties
 
 ## JSON File example
 
@@ -36,19 +56,26 @@ const init = require("easycommands.json");
   "prefix": [{
     "input": "rules", // Input to listen for. REQUIRED (If array, elements acts as aliases)
     "output": "**1.** No being rude.\n**2.** No Ads", // Output to respond. REQUIRED (If array gets random element)
-    "messageOptions": { // Discord MessageOptions. OPTIONAL
-      split: true,
-      embed: { title: "Check Rules Above!"} // Embed Object https://discordjs.guide/popular-topics/embeds.html#using-an-embed-object
+    "messageOptions": { // Discord MessageOptions with custom properties. OPTIONAL
+      "split": true,
+      "respondToSelf": false, // Custom Property which tells to respond to self or not. DEFUALT: FALSE
+      "respondToBots": true, // Custom Property which tells to respond to other bots or not. DEFUALT: TRUE
+      "respondToUsers": true, // Custom Property which tells to respond to other users or bot. DEFUALT: TRUE
+      "equalContent": false, // Custom Proerty which says to exacly equal input, or just include input. DEFAULT: TRUE
+      "embed": { "title": "Check Rules Above!" } // Embed Object https://discordjs.guide/popular-topics/embeds.html#using-an-embed-object
     },
   }],
   "noPrefix": [{
-    "input": ["Hello!", "Hey!"], //  Input to listen for. REQUIRED  (If array, elements acts as aliases)
-    "output": ["Hey!", "GoodBye"] // Output to respond with. REQUIRED (If array gets random element)
+    "input": ["Hello!", "Hey!", "Sup", "Nice to meet you"], //  Input to listen for. REQUIRED  (If array, elements acts as aliases)
+    "output": ["Hey!", "GoodBye"], // Output to respond with. REQUIRED (If array gets random element)
+    "messageOptions": { // Discord Message Options
+    "code": "json"
+    },
   }]
 }
 ```
 
-##### If copy, Remove comments from JSON example
+##### If copy, Remove code comments from JSON example
 
 ### Now either call the init function inside or outside the message event.
 
@@ -64,7 +91,7 @@ const client = new Client();
 // Outside Event
 init(client /* Client Object */, {
   // Options
-  path: "./pathToJSONCommandsFile.json", // Path to JSON file with commands. REQUIRED
+  commands: "./pathToJSONCommandsFile.json", // Path to JSON file with commands OR JS object of the shown JSON Format. REQUIRED
 
   logOnReady: true /*  Only for outside event, will log on ready event if true which will log default, or string being own custom message.
   DEFAULT: FALSE */,
@@ -73,7 +100,7 @@ init(client /* Client Object */, {
 
   reply: false, // Use message.reply if true for responding to commands. DEFAULT: FALSE
 }); // Returns passed Client object
-/* Note: init Function Adds commandsJSON property to client, value being the parsed JSON file. */
+/* Note: init Function Adds commandsJSON property to client, value being the parsed JSON file/Object. */
 ```
 
 ## Example Code Inside Message Event
@@ -88,10 +115,10 @@ client.on("message", (msg) => {
 
   /* ONLY CALL THE INIT FUNCTION ONCE */
 
-  init(client /* Instanated Client */, {
+  init(client /* Instantiated Client */, {
     // Options
 
-    path: "./pathToJSONCommandsFile.json", // Path to JSON file with commands. REQUIRED
+    commands: "./pathToJSONCommandsFile.json", // Path to JSON file. REQUIRED
 
     prefix: prefix ?? "!", // Prefix to check for the prefix property inputs. Defualt: "" (Empty String)
 
@@ -99,6 +126,124 @@ client.on("message", (msg) => {
 
     message: msg, // Message object to access the content, etc properties. REQURIED
   }); // Returns Message Object Of Reply || Passed Client
-  /* Note: init Function Adds commandsJSON property to client, value being the parsed JSON file. */
+  /* Note: init Function Adds commandsJSON property to client, value being the parsed JSON file/Object. */
 });
+```
+
+## Object
+
+## Object example
+
+```js
+// JavaScript
+const { MessageEmbed } = require("discord.js");
+
+const Commands = { // Follows same format as JSON.
+  prefix: [{
+    input: "rules", // Input to listen for. REQUIRED (If array, elements acts as aliases)
+    output: "**1.** No being rude.\n**2.** No Ads", // Output to respond. REQUIRED (If array gets random element)
+    messageOptions: { // Discord MessageOptions with custom properties. OPTIONAL
+      split: true,
+      respondToSelf: false, // Custom Property which tells to respond to self or not. DEFUALT: FALSE
+      respondToBots: true, // Custom Property which tells to respond to other bots or not. DEFUALT: TRUE
+      respondToUsers: true, // Custom Property which tells to respond to other users or bot. DEFUALT: TRUE
+      equalContent: false, // Custom Proerty which says to exacly equal input, or just include input. DEFAULT: TRUE
+      embed: new MessageEmbed().setTitle("Check Rules above") // Messagembed Constructor https://discordjs.guide/popular-topics/embeds.html#using-the-richembedmessageembed-constructor
+    },
+  }],
+  noPrefix: [{
+    input: ["Hello!", "Hey!", "Sup", "Nice to meet you"], //  Input to listen for. REQUIRED  (If array, elements acts as aliases)
+    output: ["Hey!", "GoodBye"], // Output to respond with. REQUIRED (If array gets random element)
+    messageOptions: {
+    code: "json"
+    },
+  }]
+}
+
+// TypeScript
+import init, { CommandObject } from "easycommands.json";
+
+const Commands: CommandObject /* TS Interface for command */ {
+// ...
+}
+```
+
+### Now either call the init function inside or outside the message event.
+
+## Example Code Outside Event
+
+```js
+const init = require("easycommands.json");
+const { Client, MessageEmbed } = require("discord.js");
+const client = new Client();
+
+const Commands = {
+  /* Put example object here */
+};
+
+// Outside Event
+init(client /* Instantiated Client */, {
+  // Options
+  commands: Commands, // Path to Command Object. REQUIRED
+
+  logOnReady: true /*  Only for outside event, will log on ready event if true which will log default, or string being own custom message.
+  DEFAULT: FALSE */,
+
+  prefix: "!", // Prefix to check for the prefix property inputs. Defualt: "" (Empty String)
+
+  reply: false, // Use message.reply if true for responding to commands. DEFAULT: FALSE
+}); // Returns passed Client object
+/* Note: init Function Adds commandsJSON property to client, value being the parsed JSON file/Object. */
+```
+
+## Example Code Inside Message Event
+
+```js
+const init = require("easycommands.json");
+const { Client, MessageEmbed } = require("discord.js");
+const client = new Client();
+
+const Commands = {
+  /* Put example object here */
+};
+
+client.on("message", (msg) => {
+  const prefix = ExampleDataBase.get(msg.guild.id); // Mocking DB Prefix
+
+  /* ONLY CALL THE INIT FUNCTION ONCE */
+
+  init(client /*  Instantiated Client  */, {
+    // Options
+
+    commands: Commands, // Path to Command Object. REQUIRED
+
+    prefix: prefix ?? "!", // Prefix to check for the prefix property inputs. Defualt: "" (Empty String)
+
+    reply: false, // Use message.reply if true for responding to commands. DEFAULT: FALSE
+
+    message: msg, // Message object to access the content, etc properties. REQURIED
+  }); // Returns Message Object Of Reply || Passed Client
+  /* Note: init Function Adds commandsJSON property to client, value being the parsed JSON file/Object. */
+});
+```
+
+## More info
+
+### Commands property of initOptions allow array of files and objects.
+
+```js
+  // File Paths to JSON and Objects
+  init(client, {
+    commands: ["./commands1.json". "./commands2.json", { prefix: []}, { noPrefix: []}]
+  });
+
+  // Just File Paths to JSON
+  init(client, {
+    commands: ["./commands1.json". "./commands2.json"]
+  });
+
+  // Just Objects
+  init(client, {
+    commands: [{ prefix: []}, { noPrefix: []}]
+  });
 ```
--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,50 @@
 import { Client, Message, MessageOptions } from "discord.js";
 import * as fs from "fs";
+import { type } from "os";
 
-interface Options {
-  path: string;
+export interface initOptions {
+  command: string | CommandObject | Array<string | CommandObject>;
   logOnReady?: string | boolean;
   prefix?: string;
   reply?: boolean;
   message?: Message;
 }
 
-interface Command {
+export interface Command {
   input: string | string[];
   output: string | string[];
-  messageOptions: MessageOptions;
+  messageOptions?: MessageOptions;
 }
 
-interface JSONFile {
-  prefix: Command[];
-  noPrefix: Command[];
+export interface CommandObject {
+  prefix?: Command[];
+  noPrefix?: Command[];
 }
 
 declare module "discord.js" {
   interface Client {
-    commandsJSON?: JSONFile;
+    commandsJSON?: CommandObject;
+  }
+  interface MessageOptions {
+    respondToBots?: boolean;
+    respondToSelf?: boolean;
+    respondToUsers?: boolean;
+    equalContent?: boolean;
   }
 }
 
-function init(client: Client, options: Options) {
-  const path = options.path;
+function init(client: Client, options: initOptions) {
+  const path = options.command;
   const prefix = options.prefix;
-  if (!path.endsWith(".json"))
+  if (!["string", "object"].includes(typeof path))
     throw new TypeError(
       `[Invaild Path]: Path provied either isn't a json file or doesn't excist.\nPath: ${path}`
     );
   try {
-    const objOfCommands: JSONFile = JSON.parse(
-      fs.readFileSync(
-        `${path.endsWith(".json") ? path : path + ".json"}`,
-        "utf-8"
-      )
-    );
+    let objOfCommands: CommandObject = getCommandObj(options);
     if (!objOfCommands)
       throw new TypeError(
-        `[Invaild Path]: Path provied either isn't a json file or doesn't excist.\nPath: ${path}`
+        `[Invaild Path|Object]: Path provied either isn't a json file or doesn't excist. Or object doesn't have correct properties.`
       );
     if (options.logOnReady && !options.message) {
       client.on("ready", () =>
@@ -58,59 +60,152 @@ function init(client: Client, options: Options) {
 
     function sendMessage(message: Message) {
       let found = false;
-      if (prefix && objOfCommands["prefix"]) {
-        const command = objOfCommands["prefix"].find(({ input: inputs }) => {
-          if (Array.isArray(inputs))
-            return inputs.some((input) =>
-              message.content.toLowerCase().includes(input.toLowerCase())
-            );
-          else
-            return message.content.toLowerCase().includes(inputs.toLowerCase());
-        });
+      if (
+        prefix &&
+        objOfCommands["prefix"] &&
+        message.content.toLowerCase().trim().startsWith(prefix)
+      ) {
+        const command = objOfCommands["prefix"].find(
+          ({ input: inputs, messageOptions }) => {
+            if (Array.isArray(inputs))
+              return inputs.some((input) => {
+                if (messageOptions?.equalContent === false)
+                  return message.content
+                    .toLowerCase()
+                    .includes(input.toLowerCase());
+                else
+                  return (
+                    message.content
+                      .toLowerCase()
+                      .slice(prefix.length)
+                      .trim() === input.toLowerCase().trim()
+                  );
+              });
+            else {
+              if (messageOptions?.equalContent === false)
+                return message.content
+                  .toLowerCase()
+                  .includes(inputs.toLowerCase());
+              else
+                return (
+                  message.content.toLowerCase().slice(prefix.length).trim() ===
+                  inputs.toLowerCase().trim()
+                );
+            }
+          }
+        );
         if (command) {
+          if (
+            command.messageOptions?.respondToBots === false &&
+            message.author.bot
+          )
+            return;
+          else if (
+            !command.messageOptions?.respondToSelf &&
+            message.author.id === client.user?.id
+          )
+            return;
+          else if (
+            command.messageOptions?.respondToUsers === false &&
+            !message.author.bot
+          )
+            return;
+
+          delete command.messageOptions?.respondToBots;
+          delete command.messageOptions?.respondToUsers;
+          delete command.messageOptions?.respondToSelf;
           found = true;
           const output = command.output;
           if (Array.isArray(output)) {
             options.reply
               ? message.reply(
                   output[Math.floor(Math.random() * output.length)],
-                  command.messageOptions
+                  command.messageOptions ? command.messageOptions : {}
                 )
               : message.channel.send(
                   output[Math.floor(Math.random() * output.length)],
-                  command.messageOptions
+                  command.messageOptions ? command.messageOptions : {}
                 );
           } else {
             options.reply
-              ? message.reply(command.output, command.messageOptions)
-              : message.channel.send(command.output, command.messageOptions);
+              ? message.reply(
+                  command.output,
+                  command.messageOptions ? command.messageOptions : {}
+                )
+              : message.channel.send(
+                  command.output,
+                  command.messageOptions ? command.messageOptions : {}
+                );
           }
         }
       }
-      const command = objOfCommands["noPrefix"].find(({ input: inputs }) => {
-        if (Array.isArray(inputs))
-          return inputs.some((input) =>
-            message.content.toLowerCase().includes(input.toLowerCase())
-          );
-        else
-          return message.content.toLowerCase().includes(inputs.toLowerCase());
-      });
+
+      const command = objOfCommands["noPrefix"]?.find(
+        ({ input: inputs, messageOptions }) => {
+          if (Array.isArray(inputs))
+            return inputs.some((input) => {
+              if (messageOptions?.equalContent === false)
+                return message.content
+                  .toLowerCase()
+                  .includes(input.toLowerCase());
+              else
+                return (
+                  message.content.toLowerCase().trim() === input.toLowerCase()
+                );
+            });
+          else {
+            if (messageOptions?.equalContent === false)
+              return message.content
+                .toLowerCase()
+                .includes(inputs.toLowerCase());
+            else
+              return (
+                message.content.toLowerCase().trim() ===
+                inputs.toLowerCase().trim()
+              );
+          }
+        }
+      );
       if (command && !found) {
+        if (
+          command.messageOptions?.respondToBots === false &&
+          message.author.bot
+        )
+          return;
+        else if (
+          !command.messageOptions?.respondToSelf &&
+          message.author.id === client.user?.id
+        )
+          return;
+        else if (
+          command.messageOptions?.respondToUsers === false &&
+          !message.author.bot
+        )
+          return;
+        delete command.messageOptions?.respondToBots;
+        delete command.messageOptions?.respondToUsers;
+        delete command.messageOptions?.respondToSelf;
         const output = command.output;
         if (Array.isArray(output)) {
           options.reply
             ? message.reply(
                 output[Math.floor(Math.random() * output.length)],
-                command.messageOptions
+                command.messageOptions ? command.messageOptions : {}
               )
             : message.channel.send(
                 output[Math.floor(Math.random() * output.length)],
-                command.messageOptions
+                command.messageOptions ? command.messageOptions : {}
               );
         } else {
           options.reply
-            ? message.reply(command.output, command.messageOptions)
-            : message.channel.send(command.output, command.messageOptions);
+            ? message.reply(
+                command.output,
+                command.messageOptions ? command.messageOptions : {}
+              )
+            : message.channel.send(
+                command.output,
+                command.messageOptions ? command.messageOptions : {}
+              );
         }
       }
     }
@@ -125,3 +220,39 @@ function init(client: Client, options: Options) {
 }
 
 export default init;
+
+function getCommandObj(options: initOptions): CommandObject {
+  const Command = options.command;
+  let commandObject: CommandObject = {
+    noPrefix: [],
+    prefix: [],
+  };
+
+  if (Array.isArray(Command)) {
+    Command.forEach((commandStrOrObj: any) => {
+      let command: CommandObject;
+      if (typeof commandStrOrObj === "string")
+        command = JSON.parse(fs.readFileSync(commandStrOrObj, "utf-8"));
+      else command = commandStrOrObj;
+
+      if (command?.noPrefix && commandObject?.noPrefix) {
+        for (let key in command?.noPrefix) {
+          commandObject.noPrefix.push(command.noPrefix[key]);
+        }
+      }
+
+      if (command?.prefix && commandObject?.prefix) {
+        for (let key in command.prefix) {
+          commandObject.prefix.push(command.prefix[key]);
+        }
+      }
+    });
+  } else {
+    if (typeof Command === "string")
+      commandObject = JSON.parse(fs.readFileSync(Command, "utf-8"));
+    else commandObject = Command;
+  }
+
+  //@ts-ignore
+  return commandObject;
+}
