b8f7c785162b5e102e93c052e95706b8e4cb2654
Tyson3101 <67045138+Tyson3101@users.noreply.github.com>
2020-12-10 18:10:37 +1100

    Added more options

---
+++ src/index.ts
@@ -0,0 +1,110 @@
+import { Client, Message } from "discord.js";
+import * as fs from "fs";
+
+interface Options {
+  path: string;
+  logOnReady?: string | boolean;
+  prefix?: string;
+  reply?: boolean;
+  message?: Message;
+}
+
+interface Command {
+  input: string | string[];
+  output: string | string[];
+}
+
+interface JSONFile {
+  prefix: Command[];
+  noPrefix: Command[];
+}
+
+declare module "discord.js" {
+  interface Client {
+    commandsJSON?: JSONFile;
+  }
+}
+
+function init(client: Client, options: Options) {
+  const path = options.path;
+  const prefix = options.prefix;
+  if (!path.endsWith(".json"))
+    throw new TypeError(
+      `[Invaild Path]: Path provied either isn't a json file or doesn't excist.\nPath: ${path}`
+    );
+  const objOfCommands: JSONFile = JSON.parse(
+    fs.readFileSync(
+      `${path.endsWith(".json") ? path : path + ".json"}`,
+      "utf-8"
+    )
+  );
+  if (!objOfCommands)
+    throw new TypeError(
+      `[Invaild Path]: Path provied either isn't a json file or doesn't excist.\nPath: ${path}`
+    );
+  try {
+    if (options.logOnReady && !options.message) {
+      client.on("ready", () =>
+        console.log(
+          options.logOnReady === true
+            ? "All replies loaded."
+            : options.logOnReady
+        )
+      );
+    }
+
+    client.commandsJSON = objOfCommands;
+
+    function sendMessage(message: Message) {
+      let found = false;
+      if (prefix && objOfCommands["prefix"]) {
+        const command = objOfCommands["prefix"].find(({ input: inputs }) => {
+          if (Array.isArray(inputs))
+            return inputs.some((input) => message.content.includes(input));
+          else return message.content.includes(inputs);
+        });
+        if (command) {
+          found = true;
+          const output = command.output;
+          if (Array.isArray(output)) {
+            options.reply
+              ? message.reply(output[Math.floor(Math.random() * output.length)])
+              : message.channel.send([
+                  Math.floor(Math.random() * output.length),
+                ]);
+          } else {
+            options.reply
+              ? message.reply(command.output)
+              : message.channel.send(command.output);
+          }
+        }
+      }
+      const command = objOfCommands["noPrefix"].find(({ input: inputs }) => {
+        if (Array.isArray(inputs))
+          return inputs.some((input) => message.content.includes(input));
+        else return message.content.includes(inputs);
+      });
+      if (command && !found) {
+        const output = command.output;
+        if (Array.isArray(output)) {
+          options.reply
+            ? message.reply(output[Math.floor(Math.random() * output.length)])
+            : message.channel.send([Math.floor(Math.random() * output.length)]);
+        } else {
+          options.reply
+            ? message.reply(command.output)
+            : message.channel.send(command.output);
+        }
+      }
+    }
+    if (!options.message) {
+      return client.on("message", sendMessage);
+    } else {
+      sendMessage(options.message);
+    }
+  } catch (e) {
+    throw e;
+  }
+}
+
+export default init;
